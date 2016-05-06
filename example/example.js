adc17e79d42957faaeab2d82292c11c6aba6854c
Ryan Graham <r.m.graham@gmail.com>
2016-05-06 16:36:58 -0700

    update copyright notices and license

--- example/example.js
+++ example/example.js
@@ -1,3 +1,8 @@
+// Copyright IBM Corp. 2016. All Rights Reserved.
+// Node module: loopback-connector-db2
+// This file is licensed under the Artistic License 2.0.
+// License text available at https://opensource.org/licenses/Artistic-2.0
+
 var DataSource = require('loopback-datasource-juggler').DataSource;
 var DB2 = require('../'); // loopback-connector-db2
 
--- index.js
+++ index.js
@@ -1,1 +1,6 @@
+// Copyright IBM Corp. 2016. All Rights Reserved.
+// Node module: loopback-connector-db2
+// This file is licensed under the Artistic License 2.0.
+// License text available at https://opensource.org/licenses/Artistic-2.0
+
 module.exports = require('./lib/db2.js');
