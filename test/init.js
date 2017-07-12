14949170d80ffa45d72a3a1046e50feebb0235da
ssh24 <sshasan10@hotmail.com>
2017-07-12 15:00:50 -0400

    Fix repo name env variable

--- test/init.js
+++ test/init.js
@@ -11,9 +11,10 @@ var Connector = require('../');
 var DataSource = require('loopback-datasource-juggler').DataSource;
 
 /** these are the env variables in jenkins **/
-if (process.env.CI && (process.env.BUILD_NUMBER || process.env.BUILD_ID) &&
+if (process.env.CI && process.env.PACKAGE_NAME &&
+  (process.env.BUILD_NUMBER || process.env.BUILD_ID) &&
   (process.env.nodeVersion || process.env.node)) {
-  var buildName = process.env.ghprbGhRepository.split('-')[2].toUpperCase();
+  var buildName = process.env.PACKAGE_NAME.split('-')[2].toUpperCase();
   var buildNumber = process.env.BUILD_NUMBER || process.env.BUILD_ID;
   var nodeVersion = process.env.nodeVersion || process.env.node;
   var os = process.env.OS || process.platform;
