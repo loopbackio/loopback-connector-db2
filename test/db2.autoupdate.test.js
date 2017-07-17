5328afe55472d1b960ebd74746a9e6327570e9fc
ssh24 <sshasan10@hotmail.com>
2017-07-17 15:24:03 -0400

    Add tests on autoupdate

---
+++ test/db2.autoupdate.test.js
@@ -0,0 +1,60 @@
+// Copyright IBM Corp. 2016. All Rights Reserved.
+// Node module: loopback-connector-db2
+// This file is licensed under the Artistic License 2.0.
+// License text available at https://opensource.org/licenses/Artistic-2.0
+
+'use strict';
+
+/* eslint-env node, mocha */
+process.env.NODE_ENV = 'test';
+
+require('./init.js');
+require('should');
+var assert = require('assert');
+
+var db, uniquePkIndex, properties, SimpleEmployee;
+
+describe('autoupdate', function() {
+  before(function(done) {
+    db = global.getDataSource();
+    properties = {
+      name: {
+        type: String,
+      },
+      age: {
+        type: Number,
+      },
+    };
+    SimpleEmployee = db.define('SimpleEmployee', properties);
+    db.autoupdate(function(err) {
+      assert(!err);
+      db.connector.getTableStatus('SimpleEmployee',
+        function(err, fields, indexes) {
+          assert(!err);
+          assert(fields);
+          assert(indexes);
+          uniquePkIndex = indexes[0].INDNAME;
+          done();
+        });
+    });
+  });
+
+  after(function(done) {
+    SimpleEmployee.destroyAll(done);
+  });
+
+  it('perform autoupdate and check pk index', function(done) {
+    db.autoupdate('SimpleEmployee', function(err) {
+      assert(!err);
+      db.connector.getTableStatus('SimpleEmployee',
+        function(err, fields, indexes) {
+          assert(!err);
+          assert(fields);
+          assert(indexes);
+          // a new unique name is generated upon creation of primary indexes
+          assert.notEqual(uniquePkIndex, indexes[0].INDNAME);
+          done();
+        });
+    });
+  });
+});
