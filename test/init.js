b10473aefd7594efa5ebde1117fe583780dfbd6d
Rifa Achrinza <25147899+achrinza@users.noreply.github.com>
2026-08-13 20:57:38 +0800

    ci: initial GitHub Action workflow
    see: https://github.com/loopbackio/cicd/issues/1
    Signed-off-by: Rifa Achrinza <25147899+achrinza@users.noreply.github.com>

--- test/init.js
+++ test/init.js
@@ -11,25 +11,18 @@ const Connector = require('../');
 const juggler = require('loopback-datasource-juggler');
 let DataSource = juggler.DataSource;
 
-/** these are the env variables in jenkins **/
-if (process.env.CI && process.env.PACKAGE_NAME &&
-  (process.env.BUILD_NUMBER || process.env.BUILD_ID) &&
-  (process.env.nodeVersion || process.env.node)) {
-  var buildName = process.env.PACKAGE_NAME.split('-')[2].toUpperCase();
-  var buildNumber = process.env.BUILD_NUMBER || process.env.BUILD_ID;
-  var nodeVersion = process.env.nodeVersion || process.env.node;
-  var os = process.env.OS || process.platform;
-  var schemaName = 'SCHEMA' + buildNumber +
-    '_' + buildName + '_' + os.toUpperCase() + '_' + nodeVersion;
-}
-
 var config = {
-  username: process.env.DB2_USERNAME,
-  password: process.env.DB2_PASSWORD,
+  username: process.env.DB2_USERNAME || 'db2inst1',
+  password: process.env.DB2_PASSWORD || 'password',
   hostname: process.env.DB2_HOSTNAME || 'localhost',
-  port: process.env.DB2_PORTNUM || 60000,
-  database: process.env.DB2_DATABASE || 'testdb',
+  port: process.env.DB2_PORTNUM || 50000,
+<<<<<<< Updated upstream
+  database: process.env.DB2_DATABASE || 'db2inst',
   schema: schemaName || process.env.DB2_SCHEMA || 'STRONGLOOP',
+=======
+  database: process.env.DB2_DATABASE || 'mydb',
+  schema: process.env.DB2_SCHEMA || 'STRONGLOOP',
+>>>>>>> Stashed changes
 };
 
 global.config = config;
